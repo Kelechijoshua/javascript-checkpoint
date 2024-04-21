ca9a990f77349fd733eb01036befef2530599295
Kelechijoshua <kelechijoshua04@gmail.com>
2024-04-21 13:43:07 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// addition of two numbers
+function addition(a, b) {
+	return a+b
+	
+}
+
+// cube of a number
+var a = 4
+function cubes(a) {
+	return a ** 3
+}
+
+// to get the first element in an array
+var arr = [2, 4, 76]
+function getFirstValue(arr) {
+	return arr[0]
+	
+}
